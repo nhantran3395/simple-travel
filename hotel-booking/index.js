db37620c886a56097e26271394047ec1e994be09
nhantran3395 <thanhnhan33.95@gmail.com>
2024-08-05 21:17:45 +0700

    add hotel booking lambda handler

---
+++ hotel-booking/index.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const WEBSERVICE_URL =
+  "https://sz5y4rtbza.execute-api.ap-southeast-2.amazonaws.com/Prod";
+
+async function handler(event) {
+  const payload = event.body;
+  const url = new URL(WEBSERVICE_URL + "/hotel");
+  return fetch(url, {
+    method: "POST",
+    body: payload,
+  });
+}
+
+export { handler };
