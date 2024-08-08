0fbad1de0a399b8dced1cdb03f8fab2ffa93735f
nhantran3395 <thanhnhan33.95@gmail.com>
2024-08-08 08:34:07 +0700

    add order workflow and update lambda handler

--- hotel-booking/index.js
+++ hotel-booking/index.js
@@ -4,12 +4,17 @@ const WEBSERVICE_URL =
   "https://sz5y4rtbza.execute-api.ap-southeast-2.amazonaws.com/Prod";
 
 async function handler(event) {
-  const payload = event.body;
+  console.log("triggered with payload", event);
+
   const url = new URL(WEBSERVICE_URL + "/hotel");
-  return fetch(url, {
+  const response = await fetch(url, {
     method: "POST",
-    body: payload,
+    body: JSON.stringify(event),
   });
+  const json = await response.json();
+
+  console.log("received response", json);
+  return json;
 }
 
 export { handler };
